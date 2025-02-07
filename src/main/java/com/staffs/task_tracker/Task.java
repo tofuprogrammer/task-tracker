7fe318901da10586b6a78740de6b09e43e45cae4
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-02-07 17:35:45 +0000

    Added getters and setters.

---
+++ src/main/java/com/staffs/task_tracker/Task.java
@@ -0,0 +1,53 @@
+package com.staffs.task_tracker;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private Boolean done;
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
